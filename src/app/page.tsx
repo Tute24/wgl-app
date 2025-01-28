acc3765edcd23f07bfd16a6ef5a259bccd0ebc66
Arthur Araujo <arthurtute200@hotmail.com>
2025-01-28 20:23:29 -0300

    feat: transferred the login form to a component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,36 @@
+'use client'
+
+import LoginForm from '@/components/Forms/LoginForm'
 import UnLoggedHeader from '@/components/Headers/UnLoggedHeader'
-import Link from 'next/link'
+import { ChangeEvent, FormEvent, useState } from 'react'
+
 export default function Login() {
+
+  const [usersSign, setUsersSign] = useState({
+    email:'',
+    password: ''
+  })
+
+  function loginInputHandler(event: ChangeEvent<HTMLInputElement>) {
+      setUsersSign( {
+        ...usersSign,
+        [event.target.name]: event.target.value,
+      })
+    }
+  
+    function loginSubmitHandler(event: FormEvent) {
+      event.preventDefault()
+      console.log(usersSign)
+    }
+
   return (
     <>
-      <UnLoggedHeader />
-      <div className="flex flex-col justify-center items-center min-h-screen ">
-        <div className="font-sans p-10 text-center bg-black text-white font-semibold rounded-2xl">
-          <div className="flex justify-between items-center -mt-5 mb-3"></div>
-          <form>
-            <div className="p-2">
-              <label htmlFor="email">Your E-mail: </label>
-              <input
-                className="border-solid border-2 border-amber-100 bg-amber-50 rounded-2xl text-center text-black text-sm w-full focus:outline-none ring-2 ring-amber-200 "
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Log in with your e-mail"
-              />
-            </div>
-            <div className="p-2">
-              <label htmlFor="password">Password:</label>
-              <input
-                className="border-solid border-2 border-amber-100 bg-amber-50 rounded-2xl text-center text-black text-sm w-full focus:outline-none ring-2 ring-amber-200"
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Type your password"
-              />
-            </div>
-            <div className="p-5 ">
-              <button
-                className="bg-amber-50 rounded-full py-0.5 px-4 text-black font-bold border-amber-100 border-solid border-2 w-full mt-2.5 hover:bg-amber-200 focus:outline-none ring-2 ring-amber-200"
-                type="submit"
-                id="loginButton"
-              >
-                Log In
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="text-center">
-          <span className="font-semibold">
-            Are you a new user? <Link href="/registerPage">Register now!</Link>
-          </span>
-        </div>
-      </div>
+      <UnLoggedHeader/>
+      <LoginForm
+      usersSign={usersSign}
+      onChange={loginInputHandler}
+      onSubmit={loginSubmitHandler}
+      />
     </>
   )
 }
--- src/components/Forms/RegistrationForm.tsx
+++ src/components/Forms/RegistrationForm.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { ChangeEvent, FormEvent } from "react"
 
 interface RegisterFormsProps {
