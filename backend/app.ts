59919ae719e5feb6259ac5bd22952f71895e3562
Arthur Araujo <arthurtute200@hotmail.com>
2025-02-01 20:02:53 -0300

    feat: setting up basic configs on app ts

--- backend/app.ts
+++ backend/app.ts
@@ -1,2 +1,13 @@
 import express from 'express'
 const app = express()
+import { PrismaClient } from '@prisma/client'
+require('dotenv').config()
+
+app.use(express.json())
+const prisma = new PrismaClient()
+
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>{
+    console.log(`Application running on ${port}`)
+})
