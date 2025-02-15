eeefba1e51271c079a896dafa1a54b89e64efc33
Arthur Araujo <arthurtute200@hotmail.com>
2025-02-14 23:52:29 -0300

    refactor: created the checkAuth function file to avoid code repetition

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,118 +2,79 @@
 
 import axios from 'axios'
 import LoggedHeader from '../../components/Headers/LoggedHeader'
-import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { useContextWrap } from '@/contextAPI/context'
 import List from '../../components/weddingsDisplay/List'
 import weddingProps from '@/types/weddingProps'
-import logOutFunction from '../../functions/logOutFunction'
 import useLogOut from '../../functions/logOutFunction'
+import checkAuth from '@/functions/checkAuthFunction'
 
 export default function Dashboard() {
-  const route = useRouter()
+
   const userToken = JSON.parse(localStorage.getItem('userToken') ?? 'null')
-  const {validToken,setValidToken} = useContextWrap()
-  const [ownWeddingsArray, setOwnWeddingsArray] = useState<weddingProps[]>( 
-      [{
+  const { validToken } = useContextWrap()
+  const [ownWeddingsArray, setOwnWeddingsArray] = useState<weddingProps[]>([
+    {
       id: '',
       weddingTitle: '',
       weddingDate: '',
       shippingAddress: '',
-      createdBy: ''
-    }]
-  )
+      createdBy: '',
+    },
+  ])
 
-  useEffect(()=>{
-    function checkAuth(){
-      if(userToken === null){
-        route.push('/')
-        return
-      }
-      setValidToken(true)
-    }
-    checkAuth()
-  },[])
+  checkAuth()
 
-  useEffect(()=>{
-
-    async function getWeddings (){
-      if(validToken)
-      try{
-        const response = await axios.get('http://localhost:3000/getWeddings',{headers:{
-          Authorization: `Bearer ${userToken}`
-        }})
-        if(response.status === 200){
-          setOwnWeddingsArray(response.data.own)
-        }
-      }catch(error){
-        if(axios.isAxiosError(error)){
-          if (error.response?.status === 401) {
-            console.log('User not authenticated.')
+  useEffect(() => {
+    async function getWeddings() {
+      if (validToken)
+        try {
+          const response = await axios.get(
+            'http://localhost:3000/getWeddings',
+            {
+              headers: {
+                Authorization: `Bearer ${userToken}`,
+              },
+            }
+          )
+          if (response.status === 200) {
+            setOwnWeddingsArray(response.data.own)
           }
-          if (error.response?.status === 403) {
-            console.log('Invalid/Expired token.')
-          }
-          if (error.response?.status === 404) {
-            console.log('User not found.')
-          }
-          if (error.response?.status === 500) {
-            console.log('Server error.')
+        } catch (error) {
+          if (axios.isAxiosError(error)) {
+            if (error.response?.status === 401) {
+              console.log('User not authenticated.')
+            }
+            if (error.response?.status === 403) {
+              console.log('Invalid/Expired token.')
+            }
+            if (error.response?.status === 404) {
+              console.log('User not found.')
+            }
+            if (error.response?.status === 500) {
+              console.log('Server error.')
+            }
           }
         }
-      }
     }
 
     getWeddings()
-
-  },[validToken])
+  }, [validToken])
 
   const logOut = useLogOut()
 
-  // async function LogOut() {
-
-  //   if (userToken) {
-  //     try {
-  //       const response = await axios.get('http://localhost:3000/logOut', {
-  //         headers: {
-  //           Authorization: `Bearer ${userToken}`,
-  //         },
-  //       })
-
-  //       if (response.status === 200) {
-  //         localStorage.removeItem('userToken')
-  //         route.push('/')
-  //       }
-  //     } catch (error: unknown) {
-  //       if (axios.isAxiosError(error)) {
-  //         if (error.response?.status === 401) {
-  //           console.log('User not authenticated.')
-  //         }
-  //         if (error.response?.status === 403) {
-  //           console.log('Invalid/Expired token.')
-  //         }
-  //         if (error.response?.status === 404) {
-  //           console.log('User not found.')
-  //         }
-  //         if (error.response?.status === 500) {
-  //           console.log('Server error.')
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
   return (
     <>
-    <div>
-      {validToken && (<><LoggedHeader onClick={logOut} />
-        <div>
-          <List
-          weddingsArray={ownWeddingsArray}
-          />
-        </div>  
-        </>
-    )}
-    </div>
+      <div>
+        {validToken && (
+          <>
+            <LoggedHeader onClick={logOut} />
+            <div>
+              <List weddingsArray={ownWeddingsArray} />
+            </div>
+          </>
+        )}
+      </div>
     </>
   )
 }
