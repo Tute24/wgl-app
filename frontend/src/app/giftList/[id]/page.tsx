b003847d2a43cd9b73aa9572f3acc43f57a46bad
Arthur Araujo <arthurtute200@hotmail.com>
2025-02-14 22:28:23 -0300

    feat: editing the List of wedding component

--- frontend/src/app/giftList/[id]/page.tsx
+++ frontend/src/app/giftList/[id]/page.tsx
@@ -1,7 +1,13 @@
 import LoggedHeader from "../../../components/Headers/LoggedHeader";
 
 export default function giftsList(){
+
+    function logOutfunction(){
+        console.log('logout')
+    }
     return (
-        <LoggedHeader/>
+        <LoggedHeader
+        onClick={logOutfunction}
+        />
     )
 }
--- frontend/src/components/weddingsDisplay/List.tsx
+++ frontend/src/components/weddingsDisplay/List.tsx
@@ -1,65 +1,43 @@
 import Link from 'next/link'
 
- interface ProdArrayProps {
-    _id: string
-    description: string
-    summDesc: string
-    productName: string
-    productUrl: string
-    tags: string[]
-    upVotes: number
-  }
-
-interface ListProps {
-    productsArray: ProdArrayProps[]
+interface weddingProps {
+  id: string
+  weddingTitle: string
+  weddingDate: string
+  shippingAddress?: string
+  createdBy: string
 }
 
-export default function Weddings({
-    productsArray
-}:ListProps) {
+interface weddingListProps {
+  weddingsArray: weddingProps[]
+}
+
+export default function Weddings({ weddingsArray }: weddingListProps) {
   return (
     <ul className="flex flex-col text-center items-center">
-      {productsArray.map((product) => (
+      {weddingsArray.map((wedding) => (
         <div
-          id={product.productName}
-          key={product._id}
+          id={wedding.id}
+          key={wedding.id}
           className="p-3 sm:p-5 border-gray-400 w-full sm:w-3/5"
         >
           <li
             onClick={() => {}}
-            key={product._id}
+            key={wedding.id}
             className="flex flex-col justify-center cursor-pointer border-solid border-2 shadow-md rounded-lg hover:shadow-lg hover:bg-gray-100"
           >
-            <h2 className="font-bold p-2">{product.productName}</h2>
+            <h2 className="font-bold p-2">{wedding.weddingTitle}</h2>
             <div className="flex flex-col sm:flex-row justify-between gap-4 items-center sm:ml-5 sm:mr-5 ">
-              <p className="text-xs sm:text-sm">About - {product.summDesc}</p>
+              <p className="text-xs sm:text-sm">
+                Wedding date: {wedding.weddingDate}
+              </p>
               <div className="flex flex-row items-center gap-3">
                 <Link
-                  onClick={(event) => {
-                    event.stopPropagation()
-                  }}
-                  href={product.productUrl}
-                  target="blank"
+                  href={`/giftList/${wedding.id}`}
                   className=" text-xs  text-orangeText hover:underline sm:text-base"
                 >
-                  Official Website
+                  See this wedding's gift list
                 </Link>
-
-                <input type="hidden" name="product" value={product._id} />
-                <button
-                  onClick={(event) => {
-                    event.stopPropagation()
-                  }}
-                  className="px-2 border-solid border-2 border-gray-200 rounded-md"
-                  type="button"
-                >
-                  <img
-                    className="h-5 w-5 p-0"
-                    src="/upArrow.png"
-                    alt="upVote"
-                  />
-                  <span className="text-orangeText">{product.upVotes}</span>
-                </button>
               </div>
             </div>
           </li>
