1db66e47f4e82eaae6d8095c689843426efa98a9
Arthur Araujo <arthurtute200@hotmail.com>
2025-02-18 20:58:30 -0300

    refactor: transferring some states to the contextAPI

--- frontend/src/contextAPI/context.tsx
+++ frontend/src/contextAPI/context.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import giftsProps from '@/types/giftsProps'
 import { createContext, useContext, useState } from 'react'
 
 interface ContextProps {
@@ -7,6 +8,18 @@ interface ContextProps {
   setStatusMessage: (value: string) => void
   userToken: string|null
   setUserToken: (value:string) => void
+  isGiftSent: boolean
+  setIsGiftSent: (value:boolean)=> void
+  sendGiftObj: {
+    giftID: number
+    quantity: number
+  }
+  setSendGiftObj: (value: {
+    giftID: number
+    quantity: number
+  }) => void
+  giftsArray: giftsProps[]
+  setGiftsArray: (value: giftsProps[]) => void
 }
 
 const contextWrap = createContext<ContextProps | undefined>(undefined)
@@ -14,9 +27,24 @@ const contextWrap = createContext<ContextProps | undefined>(undefined)
 export function ContextProvider({ children }: { children: React.ReactNode }) {
   const [statusMessage, setStatusMessage] = useState<string>('')
   const [userToken,setUserToken] = useState<string|null>(null)
+  const [isGiftSent, setIsGiftSent] = useState<boolean>(false)
+  const [sendGiftObj, setSendGiftObj] = useState({
+    giftID: 0,
+    quantity: 0,
+  })
+  const [giftsArray, setGiftsArray] = useState<giftsProps[]>([
+      {
+        id: 0,
+        quantity: 0,
+        productName: '',
+        productLink: '',
+        fromWedding: 0,
+        giftedBy: '',
+      },
+    ])
 
   return (
-    <contextWrap.Provider value={{ statusMessage, setStatusMessage, userToken, setUserToken }}>
+    <contextWrap.Provider value={{ statusMessage, setStatusMessage, userToken, setUserToken, isGiftSent, setIsGiftSent, sendGiftObj, setSendGiftObj, giftsArray, setGiftsArray }}>
       {children}
     </contextWrap.Provider>
   )
