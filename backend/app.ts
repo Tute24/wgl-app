85c83cfec7f00adc935bc3e4aa270c7843237a94
Arthur Araujo <arthurtute200@hotmail.com>
2025-02-20 22:17:45 -0300

    feat: created the updateGift controller and implemented its logic

--- backend/app.ts
+++ backend/app.ts
@@ -14,6 +14,7 @@ import getReqRoute from './controllers/getRequests'
 import acceptRequestRoute from './controllers/acceptRequest'
 import denyRequestRoute from './controllers/denyRequest'
 import giftPresentRouter from './controllers/giftPresent'
+import updateGiftRouter from './controllers/updateGift'
 
 app.use(express.json())
 app.use(cors())
@@ -29,6 +30,7 @@ app.use(getReqRoute)
 app.use(acceptRequestRoute)
 app.use(denyRequestRoute)
 app.use(giftPresentRouter)
+app.use(updateGiftRouter)
 
 const port = process.env.PORT || 3000
 
