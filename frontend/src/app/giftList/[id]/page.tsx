686ec4f55a2dbcbafd61cd0dee6a64991642ec61
Arthur Araujo <arthurtute200@hotmail.com>
2025-02-20 22:39:19 -0300

    feat: implemented the logic of the updateGift request on the giftList page

--- frontend/src/app/giftList/[id]/page.tsx
+++ frontend/src/app/giftList/[id]/page.tsx
@@ -10,6 +10,7 @@ import OwnerList from '../../../components/giftsListDisplay/OwnerList'
 import useGiftPresent from '@/functions/giftPresentFunction'
 import useGetData from '@/functions/getWeddingDataFunction'
 import giftsProps from '@/types/giftsProps'
+import axios from 'axios'
 
 export default function giftsList() {
   const {
@@ -46,16 +47,54 @@ export default function giftsList() {
     })
   }
 
-  function submitUpdate (){
-    console.log(updateProps)
-    setGiftsArray((prev:giftsProps[])=>(prev.map((gift)=>(updateProps.giftID===gift.id ? {...gift,
-      productLink: updateProps.productLink,
-      productName: updateProps.productName,
-      quantity: updateProps.quantity
-    } : gift))))
-    setToUpdate(false)
+  async function submitUpdate() {
+    try {
+      const response = await axios.post(
+        'http://localhost:3000/updateGift',
+        updateProps,
+        {
+          headers: {
+            Authorization: `Bearer: ${userToken}`,
+          },
+        }
+      )
+
+      if (response.status === 200) {
+        setGiftsArray((prev: giftsProps[]) =>
+          prev.map((gift) =>
+            updateProps.giftID === gift.id
+              ? {
+                  ...gift,
+                  productLink: updateProps.productLink,
+                  productName: updateProps.productName,
+                  quantity: updateProps.quantity,
+                }
+              : gift
+          )
+        )
+        setToUpdate(false)
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 401) {
+          console.log('User not authenticated.')
+        }
+        if (error.response?.status === 403) {
+          console.log(
+            `Invalid/Expired token - User is not this wedding's creator`
+          )
+        }
+        if (error.response?.status === 404) {
+          console.log('User/Gift not found.')
+        }
+        if (error.response?.status === 500) {
+          console.log('Server error.')
+        }
+      }
+      console.log(error)
     }
-  
+  }
+
   checkAuth()
 
   useGetData()
