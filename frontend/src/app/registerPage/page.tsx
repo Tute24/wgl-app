e88585a386333b9bdf25860c6b4e529140b130f4
Arthur Araujo <arthurtute200@hotmail.com>
2025-02-25 23:20:13 -0300

    refactor: implemented zod validation along with react hook form config for the new user register

--- frontend/src/app/registerPage/page.tsx
+++ frontend/src/app/registerPage/page.tsx
@@ -12,62 +12,60 @@ import { error } from 'console'
 export default function Register() {
   const { statusMessage, setStatusMessage } = useContextWrap()
 
-  const [usersData, setUsersData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  })
+  // const [usersData, setUsersData] = useState({
+  //   firstName: '',
+  //   lastName: '',
+  //   email: '',
+  //   password: '',
+  //   confirmPassword: '',
+  // })
 
-  const router = useRouter()
+  // const router = useRouter()
 
-  function UsersInputHandler(event: ChangeEvent<HTMLInputElement>) {
-    setUsersData({
-      ...usersData,
-      [event.target.name]: event.target.value,
-    })
-  }
+  // function UsersInputHandler(event: ChangeEvent<HTMLInputElement>) {
+  //   setUsersData({
+  //     ...usersData,
+  //     [event.target.name]: event.target.value,
+  //   })
+  // }
 
-  async function registrationSubmitHandler(event: FormEvent) {
-    event.preventDefault()
-    if (usersData.password !== usersData.confirmPassword) {
-      setStatusMessage('Passwords must be the same!')
-    } else{
+  // async function registrationSubmitHandler(event: FormEvent) {
+  //   event.preventDefault()
+  //   if (usersData.password !== usersData.confirmPassword) {
+  //     setStatusMessage('Passwords must be the same!')
+  //   } else{
 
-    try{
-      const response = await axios.post(
-        'http://localhost:3000/createUser',
-        usersData
-      )
+  //   try{
+  //     const response = await axios.post(
+  //       'http://localhost:3000/createUser',
+  //       usersData
+  //     )
     
-      if (response.status === 200) {
-        const userToken = response.data.token
-        localStorage.setItem('userToken',JSON.stringify(userToken))
-        router.push('/dashboard')
-      }
-    }catch(error:unknown){
-      if(axios.isAxiosError(error)){
-        if(error.response?.status === 409){
-          setStatusMessage('There is already an existent user with this email!')
-        }
-        if(error.response?.status === 500){
-          setStatusMessage('Something went wrong within the server. Try again sonn.')
-        }
-        console.log(error)
-      }
-      console.log(error)
-    }
-  }
-  }
+  //     if (response.status === 200) {
+  //       const userToken = response.data.token
+  //       localStorage.setItem('userToken',JSON.stringify(userToken))
+  //       router.push('/dashboard')
+  //     }
+  //   }catch(error:unknown){
+  //     if(axios.isAxiosError(error)){
+  //       if(error.response?.status === 409){
+  //         setStatusMessage('There is already an existent user with this email!')
+  //       }
+  //       if(error.response?.status === 500){
+  //         setStatusMessage('Something went wrong within the server. Try again sonn.')
+  //       }
+  //       console.log(error)
+  //     }
+  //     console.log(error)
+  //   }
+  // }
+  // }
 
   return (
     <>
       <UnLoggedHeader />
       <RegisterForm
-        usersData={usersData}
-        onChange={UsersInputHandler}
-        onSubmit={registrationSubmitHandler}
+        setStatusMessage={setStatusMessage}
         statusMessage={statusMessage}
       />
     </>
--- frontend/src/components/Forms/RegistrationForm.tsx
+++ frontend/src/components/Forms/RegistrationForm.tsx
@@ -1,25 +1,75 @@
-import { useContextWrap } from '../../contextAPI/context'
-import { ChangeEvent, FormEvent } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import { ChangeEvent, FormEvent, SetStateAction } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const usersDataSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email(),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8)
+})
+
+type usersData = z.infer<typeof usersDataSchema>
 
 interface RegisterFormsProps {
-  usersData: {
-    firstName: string
-    lastName: string
-    email: string
-    password: string
-    confirmPassword: string
-  }
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void
-  onSubmit: (event: FormEvent) => void
-  statusMessage: string
+  // usersData: {
+  //   firstName: string
+  //   lastName: string
+  //   email: string
+  //   password: string
+  //   confirmPassword: string
+  // }
+  // onChange: (event: ChangeEvent<HTMLInputElement>) => void
+  // onSubmit: (event: FormEvent) => void
+  statusMessage: string,
+  setStatusMessage: React.Dispatch<SetStateAction<string>>
 }
 
 export default function RegisterForm({
-  usersData,
-  onChange,
-  onSubmit,
-  statusMessage
+  statusMessage,
+  setStatusMessage
 }: RegisterFormsProps) {
+
+  const router = useRouter()
+  const {register, handleSubmit} = useForm<usersData>({
+    resolver: zodResolver(usersDataSchema)
+  })
+
+  const onSubmit: SubmitHandler<usersData> = async (data) => {
+    
+    if (data.password !== data.confirmPassword) {
+      setStatusMessage('Passwords must be the same!')
+    } else{
+
+    try{
+      const response = await axios.post(
+        'http://localhost:3000/createUser',
+        data
+      )
+    
+      if (response.status === 200) {
+        const userToken = response.data.token
+        localStorage.setItem('userToken',JSON.stringify(userToken))
+        router.push('/dashboard')
+      }
+    }catch(error:unknown){
+      if(axios.isAxiosError(error)){
+        if(error.response?.status === 409){
+          setStatusMessage('There is already an existent user with this email!')
+        }
+        if(error.response?.status === 500){
+          setStatusMessage('Something went wrong within the server. Try again sonn.')
+        }
+        console.log(error)
+      }
+      console.log(error)
+    }
+  }
+  }
 
   return (
     <>
@@ -28,7 +78,7 @@ export default function RegisterForm({
           <div className="flex justify-center items-center -mt-5 mb-3">
             <h2>Register yourself</h2>
           </div>
-          <form onSubmit={onSubmit} className="flex flex-col ">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col ">
             <div className="p-2">
               <label htmlFor="firstName">Your first name:</label>
               <input
@@ -36,9 +86,9 @@ export default function RegisterForm({
                  text-black text-sm w-full focus:outline-none ring-2 ring-amber-200 "
                 type="text"
                 id="firstName"
-                value={usersData.firstName}
-                onChange={onChange}
-                name="firstName"
+                // value={usersData.firstName}
+                // onChange={onChange}
+                {...register('firstName')}
                 required
               />
             </div>
@@ -49,9 +99,9 @@ export default function RegisterForm({
                  text-black text-sm w-full focus:outline-none ring-2 ring-amber-200 "
                 type="text"
                 id="lastName"
-                name="lastName"
-                value={usersData.lastName}
-                onChange={onChange}
+                // value={usersData.lastName}
+                // onChange={onChange}
+                {...register('lastName')}
                 required
               />
             </div>
@@ -62,9 +112,10 @@ export default function RegisterForm({
                 text-center text-black text-sm w-full focus:outline-none ring-2 ring-amber-200 "
                 type="email"
                 id="email"
-                name="email"
-                value={usersData.email}
-                onChange={onChange}
+                // name="email"
+                // value={usersData.email}
+                // onChange={onChange}
+                {...register('email')}
                 required
               />
             </div>
@@ -73,10 +124,11 @@ export default function RegisterForm({
               <input
                 className="mt-1 border-solid border-2 border-amber-100 bg-amber-50 rounded-2xl text-center text-black text-sm w-full focus:outline-none ring-2 ring-amber-200 "
                 id="password"
-                name="password"
+                // name="password"
                 type="password"
-                value={usersData.password}
-                onChange={onChange}
+                // value={usersData.password}
+                // onChange={onChange}
+                {...register('password')}
                 required
               />
             </div>
@@ -85,9 +137,10 @@ export default function RegisterForm({
               <input
                 className="mt-1 border-solid border-2 border-amber-100 bg-amber-50 rounded-2xl text-center text-black text-sm w-full focus:outline-none ring-2 ring-amber-200 "
                 type="password"
-                name="confirmPassword"
-                value={usersData.confirmPassword}
-                onChange={onChange}
+                // name="confirmPassword"
+                // value={usersData.confirmPassword}
+                // onChange={onChange}
+                {...register('confirmPassword')}
                 required
               />
             </div>
