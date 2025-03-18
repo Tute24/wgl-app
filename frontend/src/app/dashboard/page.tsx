af9d8a200bcd1d7514472fe31ba6b5ff680da552
Arthur Araujo <arthurtute200@hotmail.com>
2025-03-17 22:42:57 -0300

    refactor: improved the code with condititional rendering on the dashboard page

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -5,18 +5,20 @@ import WeddingsGuest from './(components)/weddingsDisplay/GuestWeddings'
 export default function Dashboard() {
   return (
     <>
-      <div>
-        <>
-          <LoggedHeader />
-          <div className="flex flex-row justify-center">
-            <div className="w-1/2">
-              <WeddingsOwn />
-            </div>
-            <div className="w-1/2">
-              <WeddingsGuest />
-            </div>
+      <LoggedHeader />
+      <div className="flex flex-row justify-center">
+        <div className="flex flex-col justify-center items-center w-1/2">
+          <h2 className="font-bold flex flex-col items-center">My weddings:</h2>
+          <div className="m-auto">
+            <WeddingsOwn />
           </div>
-        </>
+        </div>
+        <div className="flex flex-col items-center w-1/2">
+          <h2 className="font-bold mt-0">Weddings I'm a guest at:</h2>
+          <div className="m-auto">
+            <WeddingsGuest />
+          </div>
+        </div>
       </div>
     </>
   )
