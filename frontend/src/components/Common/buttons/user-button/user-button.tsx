eab2533989023532eadabab012f8a4982083ff97
Arthur Araujo <arthurtute200@hotmail.com>
2025-03-31 21:43:55 -0300

    refactor: changing the color behavior of the user button

--- frontend/src/components/Common/buttons/user-button/user-button.tsx
+++ frontend/src/components/Common/buttons/user-button/user-button.tsx
@@ -1,22 +1,20 @@
 interface UserButtonProps {
-    id: string
-    content: React.ReactNode
-    onClick: (e:React.MouseEvent<HTMLButtonElement>) => void
-    bg: string
-    bgHover: string
-  }
-  
-  export default function UserButton({ id, content, onClick, bg, bgHover }: UserButtonProps) {
-    return (
-      <div>
-        <button
-          className={`text-black min-w-[100px] w-full font-bold border-solid border bg-${bg} hover:${bgHover} h-10 rounded-md`}
-          type="button"
-          id={id}
-          onClick={onClick}
-        >
-          {content}
-        </button>
-      </div>
-    )
-  }
+  id: string
+  content: React.ReactNode
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+export default function UserButton({ id, content, onClick }: UserButtonProps) {
+  return (
+    <div>
+      <button
+        className={`text-black min-w-[100px] w-full font-bold border-solid border bg-mutedTaupe hover:bg-dustyRose h-10 rounded-md`}
+        type="button"
+        id={id}
+        onClick={onClick}
+      >
+        {content}
+      </button>
+    </div>
+  )
+}
