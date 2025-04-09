bd930113444d7862cdbc1380ab6e3402e0447c9e
Arthur Araujo <arthurtute200@hotmail.com>
2025-04-08 22:04:01 -0300

    feat: implementing the useGetGiftedProducts request call on the page

--- frontend/src/app/giftsTable/[id]/page.tsx
+++ frontend/src/app/giftsTable/[id]/page.tsx
@@ -1,6 +1,21 @@
-export default function giftTablePage(){
-    return(
-        <>
-        </>
-    )
+'use client'
+
+import { useEffect } from 'react'
+import useGetGiftedProducts from './(hooks)/useGetGiftedProducts'
+import LoggedHeader from '@/components/Headers/LoggedHeader'
+
+export default function giftTablePage() {
+  const giftedProductsArray = useGetGiftedProducts()
+
+  useEffect(() => {
+    console.log(giftedProductsArray)
+  }, [giftedProductsArray])
+  return (
+    <>
+      <LoggedHeader />
+      <div>
+        Oi
+      </div>
+    </>
+  )
 }
