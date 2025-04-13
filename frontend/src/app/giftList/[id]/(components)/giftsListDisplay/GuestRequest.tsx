b72bf3fefc160a7975176911ed55aee36aefeba8
Arthur Araujo <arthurtute200@hotmail.com>
2025-04-13 17:52:17 -0300

    refactor: changing the button on the request page to the component ones

--- frontend/src/app/giftList/[id]/(components)/giftsListDisplay/GuestRequest.tsx
+++ frontend/src/app/giftList/[id]/(components)/giftsListDisplay/GuestRequest.tsx
@@ -3,39 +3,38 @@
 import { useContextWrap } from '@/contextAPI/context'
 import useMakeRequest from '@/app/giftList/[id]/(hooks)/useMakeRequest'
 import Link from 'next/link'
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
+import UserButton from '@/components/Common/buttons/user-button/user-button'
 
 export default function GuestRequest() {
   const { id } = useParams()
   const weddingID = Number(id)
   const { statusMessage, setNotGuest, isCreator, notGuest } = useContextWrap()
   const makeRequest = useMakeRequest(weddingID)
+  const route = useRouter()
   if (notGuest && !isCreator) {
     return (
       <>
         <div className="flex flex-col items-center justify-center">
-          <h2>
-            Not a guest of this wedding, make a request or go back to the
-            initial page
+          <h2 className='text-amber-800 font-bold'>
+            Not a guest of this wedding currently. You can make a request or go back to the
+            initial page.
           </h2>
           <div className="flex flex-col">
-            <div className="py-3 flex flex-row justify-center gap-2">
-              <Link href="/dashboard">
-                <button
-                  onClick={() => {
-                    setNotGuest(false)
-                  }}
-                  className="font-semibold border-solid border-gray-200 border-2 rounded-3xl px-5 py-2 mr-5 hover:bg-gray-200"
-                >
-                  <span className="text-xs">Go back to the dashboard</span>
-                </button>
-              </Link>
-              <button
-                onClick={makeRequest}
-                className="px-5 py-2 bg-black text-white font-bold rounded-3xl hover:bg-black"
-              >
-                <span className="text-xs">Request Access to the list</span>
-              </button>
+            <div className="pt-5 flex flex-row justify-center gap-8">
+              <UserButton
+              className='!bg-warmBeige !w-[225px]'
+              content='Go back to the dashboard'
+              onClick={()=>{
+                setNotGuest(false)
+                route.push('/dashboard')
+              }}
+              />
+              <UserButton
+              className='!w-[225px]'
+              content='Request access to the list'
+              onClick={makeRequest}
+              />
             </div>
             <div>
               <span className="font-bold text-xs py-3">{statusMessage}</span>
