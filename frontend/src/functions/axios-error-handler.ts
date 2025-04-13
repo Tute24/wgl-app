e6578989d2e5cbcb92239b92eebaa688621fac15
Arthur Araujo <arthurtute200@hotmail.com>
2025-04-13 19:10:13 -0300

    feat: implemented the statusMessage treatment on the axios-error-handler

--- frontend/src/functions/axios-error-handler.ts
+++ frontend/src/functions/axios-error-handler.ts
@@ -15,16 +15,22 @@ export default function AxiosErrorHandler({
   notGuest,
   setNotGuest,
   setStatusMessage,
-  route
+  route,
 }: AxiosErrorHandlerProps) {
   if (axios.isAxiosError(error)) {
     if (error.response?.status === 401) {
       console.log('User not authenticated.')
+      if (setStatusMessage !== undefined) {
+        setStatusMessage(`User doesn't have permission.`)
+      }
     }
     if (error.response?.status === 403) {
       console.log('Invalid/Expired token.')
-      if (setNotGuest!== undefined && !notGuest) {
+      if (setNotGuest !== undefined && !notGuest) {
         setNotGuest(true)
+      }
+      if (setStatusMessage !== undefined) {
+        setStatusMessage(`Invalid credentials.`)
       }
     }
     if (error.response?.status === 404) {
@@ -33,6 +39,9 @@ export default function AxiosErrorHandler({
     }
     if (error.response?.status === 500) {
       console.log('Server error.')
+      if (setStatusMessage !== undefined) {
+        setStatusMessage(`Something went wrong. Try again.`)
+      }
     }
   }
 }
