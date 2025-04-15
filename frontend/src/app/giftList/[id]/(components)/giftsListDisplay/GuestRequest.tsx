1e0b5abe6e9bf5a201b80dcec505397eeb8b2f79
Arthur Araujo <arthurtute200@hotmail.com>
2025-04-14 23:32:28 -0300

    feat: implemented the wedding header on the guestrequest component

--- frontend/src/app/giftList/[id]/(components)/giftsListDisplay/GuestRequest.tsx
+++ frontend/src/app/giftList/[id]/(components)/giftsListDisplay/GuestRequest.tsx
@@ -9,15 +9,19 @@ import WeddingHeader from '../wedding-header/wedding-header'
 export default function GuestRequest() {
   const { id } = useParams()
   const weddingID = Number(id)
-  const { statusMessage, setNotGuest, weddingData } =
-    useContextWrap()
+  const { statusMessage, setNotGuest, weddingHeaderInfo } = useContextWrap()
   const makeRequest = useMakeRequest(weddingID)
   const route = useRouter()
 
   return (
     <>
       <div className="flex flex-col items-center justify-center">
-        <WeddingHeader owner={false} />
+        <WeddingHeader
+          owner={false}
+          weddingDate={weddingHeaderInfo.weddingDate}
+          weddingTitle={weddingHeaderInfo.weddingTitle}
+          id={weddingHeaderInfo.id}
+        />
         <h2 className="font-bold text-amber-800">
           Not a guest of this wedding currently. You can make a request or go
           back to the initial page.
