7f42c9ba27bec3e4dedcf8f0be324800dc3d9857
Arthur Araujo <arthurtute200@hotmail.com>
2025-04-28 22:19:55 -0300

    feat: created the forgot-password page

---
+++ frontend/src/app/forgot-password/page.tsx
@@ -0,0 +1,62 @@
+'use client'
+import UserButton from '@/components/Common/buttons/user-button/user-button'
+import InputContainer from '@/components/Common/input-container/input-container'
+import UnLoggedHeader from '@/components/Headers/UnLoggedHeader'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const emailSchema = z.object({
+  email: z.string().email(),
+})
+
+type userEmail = z.infer<typeof emailSchema>
+
+export default function SendMail() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<userEmail>({
+    resolver: zodResolver(emailSchema),
+  })
+
+  const onSubmit: SubmitHandler<userEmail> = (data) => {
+    console.log(data)
+  }
+  return (
+    <>
+      <UnLoggedHeader />
+      <div className="flex flex-col justify-center m-auto items-center">
+        <div className=" w-[350px] border-solid border-2 border-dustyRose rounded-lg hover:shadow-xl hover:shadow-dustyRose">
+          <div>
+            <h1 className="text-amber-800 font-bold text-start px-3 py-2 text-xl">
+              FORGOT YOUR PASSWORD?
+            </h1>
+            <hr className="border-solid border-1 border-mutedTaupe py-2" />
+            <h2 className="text-amber-800 font-bold flex justify-start pb-3 px-3">
+              Enter your email and we'll send you password reset instructions.
+            </h2>
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="pb-3 px-3">
+              <InputContainer
+                {...register('email')}
+                label="Email Address"
+                type="email"
+              />
+              {errors.email && (
+                <span className="text-red-500 font-bold">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+            <div className="pb-3 px-3">
+              <UserButton content="Confirm" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
