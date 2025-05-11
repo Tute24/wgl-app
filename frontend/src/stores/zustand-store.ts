fc280c8aa49f2044af72ea663addf03337574893
Arthur Araujo <arthurtute200@hotmail.com>
2025-05-11 18:36:09 -0300

    feat: implementing the persist middleware on useUSernameStore

--- frontend/src/stores/zustand-store.ts
+++ frontend/src/stores/zustand-store.ts
@@ -1,10 +1,18 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 type UsernameStoreState = { username: string | null }
 type UsernameStoreAction = {
   setUsername: (nextUsername: UsernameStoreState['username']) => void
 }
 type UsernameStore = UsernameStoreState & UsernameStoreAction
-export const useUsernameStore = create<UsernameStore>((set) => ({
-  username: null,
-  setUsername: (nextUsername) => set({ username: nextUsername }),
-}))
+export const useUsernameStore = create<UsernameStore>()(
+  persist(
+    (set) => ({
+      username: null,
+      setUsername: (nextUsername) => set({ username: nextUsername }),
+    }),
+    {
+      name: 'username-storage',
+    }
+  )
+)
