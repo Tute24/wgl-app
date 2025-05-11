e719f1dda9a9316434c7107c478932b62cda2e85
Arthur Araujo <arthurtute200@hotmail.com>
2025-05-11 18:15:55 -0300

    feat: updating the username zustand state on the useGetOwnWedding hook

--- frontend/src/app/dashboard/(hooks)/useGetOwnWeddings.ts
+++ frontend/src/app/dashboard/(hooks)/useGetOwnWeddings.ts
@@ -2,13 +2,16 @@
 
 import { useContextWrap } from '@/contextAPI/context'
 import AxiosErrorHandler from '@/functions/axios-error-handler'
+import { useUsernameStore } from '@/stores/zustand-store'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
+import { useStore } from 'zustand'
 
 export default function useGetOwnWeddings() {
   const { userToken, setOwnWeddingsArray } = useContextWrap()
   const route = useRouter()
+  const setUsername = useStore(useUsernameStore, (state) => state.setUsername)
   useEffect(() => {
     async function getWeddings() {
       if (userToken)
@@ -23,6 +26,7 @@ export default function useGetOwnWeddings() {
           )
           if (response.status === 200) {
             setOwnWeddingsArray(response.data.own)
+            setUsername(response.data.userInfo.userName)
           }
         } catch (error) {
           AxiosErrorHandler({ error, route })
