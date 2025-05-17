3a07f27f8e825292215c9c28e1d6088941a66d49
Arthur Araujo <arthurtute200@hotmail.com>
2025-05-17 19:14:43 -0300

    feat: implemented the logic of the delete-wedding-modals usage

--- frontend/src/app/dashboard/(components)/weddingsDisplay/OwnWeddings.tsx
+++ frontend/src/app/dashboard/(components)/weddingsDisplay/OwnWeddings.tsx
@@ -3,28 +3,47 @@
 import { useContextWrap } from '@/contextAPI/context'
 import useGetOwnWeddings from '../../(hooks)/useGetOwnWeddings'
 import WeddingCard from '../weddingCard/wedding-card'
+import DeleteWeddingModal from '@/components/modals/delete-weddings-modal'
+import useDeleteWedding from '../../(hooks)/useDeleteWedding'
 
 export default function WeddingsOwn() {
-  const { ownWeddingsArray } = useContextWrap()
+  function closeModal() {
+    setModalObject({
+      id: 0,
+      name: '',
+      isOpen: false,
+    })
+  }
+  const { ownWeddingsArray, modalObject, setModalObject } = useContextWrap()
+  const { id, name, isOpen } = modalObject
   useGetOwnWeddings()
+  const deleteWedding = useDeleteWedding()
   if (ownWeddingsArray.length > 0) {
     return (
-      <ul className="flex flex-col text-center items-center">
-        {ownWeddingsArray.map((wedding) => (
-          <div
-            id={`${wedding.id}`}
-            key={wedding.id}
-            className="p-3 sm:p-5 border-gray-400 w-full flex flex-row"
-          >
+      <>
+        <ul className="flex flex-col text-center items-center gap-5">
+          {ownWeddingsArray.map((wedding) => (
             <WeddingCard
+              key={wedding.id}
               id={wedding.id}
               title={wedding.weddingTitle}
               date={wedding.weddingDate.replace(/-/g, '/')}
               isOwn={true}
             />
-          </div>
-        ))}
-      </ul>
+          ))}
+        </ul>
+        <div>
+          {modalObject && (
+            <DeleteWeddingModal
+              wedding={name}
+              onDelete={deleteWedding}
+              onCloseModal={closeModal}
+              isOpen={isOpen}
+              id={id}
+            />
+          )}
+        </div>
+      </>
     )
   } else {
     return (
