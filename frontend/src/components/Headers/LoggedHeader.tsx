56784f4f9f5a06d39685d38ca3d729421e5fb02c
Arthur Araujo <arthurtute200@hotmail.com>
2025-06-22 20:41:40 -0300

    refactor: refactoring the logged header

--- frontend/src/components/Headers/LoggedHeader.tsx
+++ frontend/src/components/Headers/LoggedHeader.tsx
@@ -4,9 +4,25 @@ import { useContextWrap } from '@/contextAPI/context'
 import checkAuth from '@/functions/useCheckAuth'
 import useLogOut from '@/functions/sign-out'
 import Link from 'next/link'
-import UserButton from '../Common/buttons/user-button/user-button'
 import { useStore } from 'zustand'
 import { useUsernameStore } from '@/stores/zustand-store'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import {
+  Menu,
+  LogOut,
+  Hourglass,
+  ListPlus,
+  LayoutDashboard,
+  ShieldQuestionIcon,
+} from 'lucide-react'
+import Image from 'next/image'
 
 export default function LoggedHeader() {
   checkAuth()
@@ -15,16 +31,19 @@ export default function LoggedHeader() {
   const username = useStore(useUsernameStore, (state) => state.username)
   return (
     <>
-      <div className="py-5 bg-softWhite">
-        <nav className=" flex text-sm items-center justify-evenly flex-row w-full sm:text-base">
-          <div className="text-amber-800 hover:text-amber-900 font-bold">
-            <Link href="aboutPage">
-              <button type="button" className="p-2">
-                About
-              </button>
+      <div className="py-5 bg-stone-100 font-poppins">
+        <nav className=" flex text-sm items-center justify-evenly flex-row w-full sm:text-xl">
+          <div className="-mt-5 -mb-8 lg:-mt-10 lg:-mb-10">
+            <Link href="/dashboard">
+              <Image
+                src="/reworked-logo.png"
+                alt="Logo"
+                width={100}
+                height={100}
+              />
             </Link>
           </div>
-          <div className="text-amber-800 hover:text-amber-900 font-bold">
+          <div className="text-amber-800 hover:text-amber-900 font-bold hidden lg:block">
             <Link href="/dashboard">
               <button
                 onClick={() => {
@@ -34,11 +53,11 @@ export default function LoggedHeader() {
                 type="button"
                 className="p-2"
               >
-                My lists
+                Weddings Dashboard
               </button>
             </Link>
           </div>
-          <div className="text-amber-800 hover:text-amber-900 font-bold">
+          <div className="text-amber-800 hover:text-amber-900 font-bold hidden lg:block">
             <Link href="/createList">
               <button
                 type="button"
@@ -47,28 +66,100 @@ export default function LoggedHeader() {
                   setStatusMessage('')
                 }}
               >
-                New wedding gift list
+                Create New Wedding
               </button>
             </Link>
           </div>
           <div className="flex flex-row gap-5 items-center">
-            <span className="text-mutedTaupe font-bold underline-offset-0">
+            <span className="text-stone-500 font-bold underline-offset-0 text-xl">
               Welcome, {username}
             </span>
-            <Link href="/">
-              <UserButton
-                className="!w-[80px] sm:!w-[150px]"
-                content="Log Out"
-                onClick={() => {
-                  logOut()
-                  setStatusMessage('')
-                }}
-              />
-            </Link>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Menu className="text-amber-800" size={28} />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="font-medium text-amber-800 px-3 bg-stone-100">
+                <DropdownMenuLabel className="text-xl font-bold font-poppins">
+                  Options
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator className="bg-amber-800 -mx-3" />
+                <Link href="/dashboard">
+                  <DropdownMenuItem className="lg:hidden">
+                    <button
+                      className="flex flex-row gap-2 items-center"
+                      type="button"
+                      onClick={() => {
+                        setStatusMessage('')
+                      }}
+                    >
+                      <LayoutDashboard size={24} />
+                      <span className="text-lg font-poppins">Dashboard</span>
+                    </button>
+                  </DropdownMenuItem>
+                </Link>
+                <Link href="/createList">
+                  <DropdownMenuItem className="lg:hidden">
+                    <button
+                      className="flex flex-row gap-2 items-center"
+                      type="button"
+                      onClick={() => {
+                        setStatusMessage('')
+                      }}
+                    >
+                      <ListPlus size={24} />
+                      <span className="text-lg font-poppins">
+                        Create new wedding
+                      </span>
+                    </button>
+                  </DropdownMenuItem>
+                </Link>
+                <DropdownMenuItem>
+                  <button
+                    className="flex flex-row gap-2 items-center"
+                    type="button"
+                    onClick={() => {
+                      setStatusMessage('')
+                    }}
+                  >
+                    <Hourglass size={24} />
+                    <span className="text-lg font-poppins">
+                      Pending Requests
+                    </span>
+                  </button>
+                </DropdownMenuItem>
+                <Link href="/aboutPage">
+                  <DropdownMenuItem>
+                    <button
+                      className="flex flex-row gap-2 items-center"
+                      type="button"
+                      onClick={() => {
+                        setStatusMessage('')
+                      }}
+                    >
+                      <ShieldQuestionIcon size={24} />
+                      <span className="text-lg font-poppins">About Us</span>
+                    </button>
+                  </DropdownMenuItem>
+                </Link>
+                <DropdownMenuItem>
+                  <button
+                    className="flex flex-row gap-2 items-center"
+                    type="button"
+                    onClick={() => {
+                      logOut()
+                      setStatusMessage('')
+                    }}
+                  >
+                    <LogOut size={24} />
+                    <span className="text-lg font-poppins">Sign Out</span>
+                  </button>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </nav>
       </div>
-      <hr className="mb-5 border-2 border-mutedTaupe" />
+      <hr className="mb-5 border-2 border-amber-800" />
     </>
   )
 }
