62d434e168be9de6b25efa379268f583984a949f
Arthur Araujo <arthurtute200@hotmail.com>
2025-07-01 21:31:38 -0300

    refactor: implementing shadcn dropdown menu on the unlogged header component

--- frontend/src/components/Headers/unlogged-header.tsx
+++ frontend/src/components/Headers/unlogged-header.tsx
@@ -3,15 +3,22 @@
 import Link from 'next/link'
 import { useContextWrap } from '@/contextAPI/context'
 import Image from 'next/image'
-import { Button } from '../ui/button'
-import { ShieldQuestionIcon } from 'lucide-react'
+import { Menu, ShieldQuestionIcon, LogIn, UserPlus } from 'lucide-react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
 
 export default function UnLoggedHeader() {
   const { setStatusMessage } = useContextWrap()
   return (
     <>
       <div className="flex flex-row py-5 bg-stone-100 font-poppins max-h-[85px] text-center items-center w-full">
-        <nav className=" flex text-sm items-center justify-between gap-10 lg:justify-between flex-row w-full sm:text-xl">
+        <nav className=" flex items-center justify-between lg:justify-between flex-row w-full px-5 sm:text-xl">
           <div className="flex flex-row items-center lg:gap-20 lg:px-20">
             <div className="relative w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-100 lg:h-100 mt-1">
               <Link href="/">
@@ -24,46 +31,64 @@ export default function UnLoggedHeader() {
                 />
               </Link>
             </div>
-            <div className="text-amber-800 hover:text-amber-900 hidden lg:block font-bold">
-              <Link href="/aboutPage">About Us</Link>
-            </div>
           </div>
           <div className="flex flex-row items-center gap-5 lg:gap-20 text-center lg:px-20">
-            <div className="">
-              <Link href="/">
-                <Button
-                  type="button"
-                  onClick={() => setStatusMessage('')}
-                  className="text-md lg:text-lg font-poppins font-bold text-stone-700 bg-paleGold hover:bg-dustyRose"
-                >
-                  Sign In
-                </Button>
-              </Link>
-            </div>
-            <div>
-              <Link href="/registerPage">
-                <Button
-                  type="button"
-                  onClick={() => setStatusMessage('')}
-                  className="text-md lg:text-lg font-poppins font-bold text-stone-700 bg-champagneGold hover:bg-paleGold"
-                >
-                  Sign Up
-                </Button>
-              </Link>
-            </div>
-          </div>
-          <div className="block lg:hidden items-center text-center pr-4 mt-1">
-            <Link href="/aboutPage">
-              <button
-                className="items-center"
-                type="button"
-                onClick={() => {
-                  setStatusMessage('')
-                }}
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Menu className="text-amber-800" size={36} />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                align="end"
+                className="font-medium text-amber-800 px-3 bg-white"
               >
-                <ShieldQuestionIcon size={24} className="text-amber-800" />
-              </button>
-            </Link>
+                <DropdownMenuLabel className="text-xl font-bold font-poppins">
+                  Options
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator className="bg-amber-800 -mx-3" />
+                <Link href="/">
+                  <DropdownMenuItem>
+                    <button
+                      className="flex flex-row gap-2 items-center"
+                      type="button"
+                      onClick={() => {
+                        setStatusMessage('')
+                      }}
+                    >
+                      <LogIn size={24} />
+                      <span className="text-lg font-poppins">Sign In</span>
+                    </button>
+                  </DropdownMenuItem>
+                </Link>
+                <Link href="/registerPage">
+                  <DropdownMenuItem>
+                    <button
+                      className="flex flex-row gap-2 items-center"
+                      type="button"
+                      onClick={() => {
+                        setStatusMessage('')
+                      }}
+                    >
+                      <UserPlus size={24} />
+                      <span className="text-lg font-poppins">Sign Up</span>
+                    </button>
+                  </DropdownMenuItem>
+                </Link>
+                <Link href="/aboutPage">
+                  <DropdownMenuItem>
+                    <button
+                      className="flex flex-row gap-2 items-center"
+                      type="button"
+                      onClick={() => {
+                        setStatusMessage('')
+                      }}
+                    >
+                      <ShieldQuestionIcon size={24} />
+                      <span className="text-lg font-poppins">About Us</span>
+                    </button>
+                  </DropdownMenuItem>
+                </Link>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </nav>
       </div>
