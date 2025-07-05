51c5ff822855cf3745a22c6c945aa0b0be8c9e8d
Arthur Araujo <arthurtute200@hotmail.com>
2025-07-05 16:18:31 -0300

    refactor: refactoring the giftsSchema for a better validation with zod

--- frontend/src/zodSchemas/giftsSchema.tsx
+++ frontend/src/zodSchemas/giftsSchema.tsx
@@ -13,8 +13,12 @@ export default giftsSchema
 export const newGiftsSchema = z.object({
   gifts: z.array(
     z.object({
-      productName: z.string(),
-      productLink: z.string(),
+      productName: z
+        .string()
+        .min(2, { message: 'Enter a valid name for the product' }),
+      productLink: z
+        .string()
+        .min(3, { message: 'Enter a valid link for the gift' }),
       quantity: z.string(),
     }),
   ),
