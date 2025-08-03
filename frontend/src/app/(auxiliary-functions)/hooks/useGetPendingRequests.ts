bdc467eed848dbe1f4a82f57c397b3988d290c61
Arthur Araujo <arthurtute200@hotmail.com>
2025-08-03 18:16:24 -0300

    feat: created the useGetPendingRequests hook

---
+++ frontend/src/app/(auxiliary-functions)/hooks/useGetPendingRequests.ts
@@ -0,0 +1,28 @@
+import { useGeneralStore } from '@/stores/general/general.provider'
+import { useShallow } from 'zustand/shallow'
+import AxiosErrorHandler from '../axios-error-handler'
+import { AxiosApi } from '@/common/axios-api/axios-api'
+
+export function useGetPendingRequests() {
+  const { setPendingRequests } = useGeneralStore(
+    useShallow((store) => ({
+      setPendingRequests: store.setPendingRequests,
+    })),
+  )
+
+  async function getPendingRequests() {
+    try {
+      const response = await AxiosApi({
+        httpMethod: 'get',
+        route: '/requests/pending-requests',
+      })
+
+      if (response.status === 200) {
+        setPendingRequests(response.data.pendingRequests)
+      }
+    } catch (error) {
+      AxiosErrorHandler({ error })
+    }
+  }
+  return getPendingRequests
+}
