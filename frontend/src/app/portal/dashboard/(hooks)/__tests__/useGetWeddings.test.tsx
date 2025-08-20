bd1571f288ec419e680074358bc38a79b0953925
Arthur Araujo <arthurtute200@hotmail.com>
2025-08-19 22:08:08 -0300

    test: unitary test for the useGetWedding hook

--- frontend/src/app/portal/dashboard/(hooks)/__tests__/useGetWeddings.test.tsx
+++ frontend/src/app/portal/dashboard/(hooks)/__tests__/useGetWeddings.test.tsx
@@ -5,15 +5,20 @@ import { AxiosApi } from '@/common/axios-api/axios-api'
 import { weddingsMock } from '../../__mocks__/weddingsMock'
 import { act, renderHook } from '@testing-library/react'
 import useGetWeddings from '../useGetWeddings'
+import AxiosErrorHandler from '@/app/(auxiliary-functions)/axios-error-handler'
+import { useRouter } from 'next/navigation'
 
 vi.mock('@/common/axios-api/axios-api')
 vi.mock('@/stores/weddings/weddings.provider')
 vi.mock('@/stores/general/general.provider')
 vi.mock('next/navigation')
+vi.mock('@/app/(auxiliary-functions)/axios-error-handler')
 
 const mockUseWeddingsStore = useWeddingsStore as Mock<typeof useWeddingsStore>
 const mockUseGeneralStore = useGeneralStore as Mock<typeof useGeneralStore>
 const mockAxiosApi = AxiosApi as Mock
+const mockAxiosErrorHandler = AxiosErrorHandler as Mock
+const mockUseRouter = useRouter as Mock
 
 describe('useGetWeddings', () => {
   it('calls the hook successfully and populates the zustand states', async () => {
@@ -45,6 +50,28 @@ describe('useGetWeddings', () => {
     expect(mockSetInvitedWeddings).toHaveBeenCalledWith([
       weddingsMock.guestWeddings,
     ])
+    expect(mockSetIsRendering).toHaveBeenNthCalledWith(2, false)
+  })
+
+  it('mocks a situation where the api returns an error', async () => {
+    const mockRouter = vi.fn()
+    mockUseRouter.mockReturnValue({ router: mockRouter })
+    const mockSetIsRendering = vi.fn()
+    mockUseGeneralStore.mockReturnValue({ setIsRendering: mockSetIsRendering })
+    const mockedError = new Error('Error')
+
+    mockAxiosApi.mockRejectedValue(mockedError)
+
+    const { result } = renderHook(() => useGetWeddings())
+
+    await act(async () => {
+      await result.current()
+    })
+    expect(mockSetIsRendering).toHaveBeenNthCalledWith(1, true)
+    expect(mockAxiosErrorHandler).toHaveBeenCalledWith({
+      route: { router: mockRouter },
+      error: mockedError,
+    })
     expect(mockSetIsRendering).toHaveBeenNthCalledWith(2, false)
   })
 })
