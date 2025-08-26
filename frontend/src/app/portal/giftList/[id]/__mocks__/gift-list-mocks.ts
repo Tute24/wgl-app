a7dd2e63cfd3bb97ca000130be1a8bae5b74eef8
Arthur Araujo <arthurtute200@hotmail.com>
2025-08-25 23:07:41 -0300

    test: created the giftListMocks for testing

---
+++ frontend/src/app/portal/giftList/[id]/__mocks__/gift-list-mocks.ts
@@ -0,0 +1,21 @@
+export const giftListMocks = {
+  listHeader: {
+    weddingId: 1,
+    listHeaderTitle: 'Wedding1',
+    listHeaderDate: '01-01-2026',
+  },
+  weddingGifts: [
+    {
+      Id: 1,
+      quantity: 2,
+      productName: 'Product1',
+      productLink: 'https://wwww.product1.com',
+    },
+    {
+      Id: 2,
+      quantity: 1,
+      productName: 'Product2',
+      productLink: 'https://wwww.product2.com',
+    },
+  ],
+}
