e8e00930627bf27d371699cfe21151f42f620822
Arthur Araujo <arthurtute200@hotmail.com>
2025-10-07 23:02:42 -0300

    test: implementing the unitary test for the useGetGiftedProducts hook

--- frontend/src/app/portal/giftList/[id]/__mocks__/gift-list-mocks.ts
+++ frontend/src/app/portal/giftList/[id]/__mocks__/gift-list-mocks.ts
@@ -18,4 +18,20 @@ export const giftListMocks = {
       productLink: 'https://wwww.product2.com',
     },
   ],
+  mappingAddGifter: [
+    {
+      id: 1,
+      presenter: 'John Doe',
+      quantityGifted: 1,
+      gift: 'Product 1',
+      giftedAt: '01-01-2025',
+    },
+    {
+      id: 2,
+      presenter: 'Jane Doe',
+      quantityGifted: 2,
+      gift: 'Product 2',
+      giftedAt: '02-02-2025',
+    },
+  ],
 }
