9311ebdd421fe658df77ceceff66217dc2db0693
Arthur Araujo <arthurtute200@hotmail.com>
2025-10-22 22:39:52 -0300

    test: implementing the useSubmitList hook unitary test

---
+++ frontend/src/app/portal/createList/__mocks__/weddingsMock.ts
@@ -0,0 +1,22 @@
+import { WeddingsProps } from '@/stores/weddings/weddings.store'
+
+export const mockWedding: WeddingsProps = {
+  id: 1,
+  weddingTitle: 'Wedding1',
+  weddingDate: '01-01-2025',
+  shippingAddress: 'Street1',
+  createdBy: 'User1',
+}
+
+export const mockNewList = {
+  listTitle: 'Wedding1',
+  weddingDate: '01-01-2025',
+  shippingAddress: 'Street1',
+  gifts: [
+    {
+      productName: 'Gift1',
+      productLink: 'https://gift1.com',
+      quantity: '1',
+    },
+  ],
+}
