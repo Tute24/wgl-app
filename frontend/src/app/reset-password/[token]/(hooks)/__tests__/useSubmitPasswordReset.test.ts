d1547f977c116d5b699fee76d9aec2e1b95e01fd
Arthur Araujo <arthurtute200@hotmail.com>
2025-11-07 23:20:51 -0300

    test:implementing unitary test for the useSubmitPasswordReset hook

--- frontend/src/app/reset-password/[token]/(hooks)/__tests__/useSubmitPasswordReset.test.ts
+++ frontend/src/app/reset-password/[token]/(hooks)/__tests__/useSubmitPasswordReset.test.ts
@@ -1,11 +1,13 @@
 import { useGeneralStore } from '@/stores/general/general.provider'
 import { act, renderHook } from '@testing-library/react'
-import { describe, expect, it, Mock, vi } from 'vitest'
-import useSendPasswordResetRequest from '../useSendPasswordResetRequest'
+import { beforeEach, describe, expect, it, Mock, vi } from 'vitest'
 import AxiosErrorHandler from '@/app/(auxiliary-functions)/axios-error-handler'
 import axios from 'axios'
+import useSubmitPasswordReset from '../useSubmitPasswordReset'
+import { useParams } from 'next/navigation'
+import { newPassword } from '../../page'
 
-vi.mock('@/common/axios-api/axios-api')
+vi.mock('next/navigation')
 vi.mock('axios')
 vi.mock('@/stores/general/general.provider')
 vi.mock('@/app/(auxiliary-functions)/axios-error-handler')
@@ -13,12 +15,20 @@ vi.mock('@/app/(auxiliary-functions)/axios-error-handler')
 const mockUseGeneralStore = useGeneralStore as Mock<typeof useGeneralStore>
 const mockAxiosPost = axios.post as unknown as Mock
 const mockAxiosErrorHandler = AxiosErrorHandler as Mock
+const mockUseParams = useParams as Mock
 
-const mockData = {
-  email: 'example@test.com',
+const token = 'token'
+
+const mockData: newPassword = {
+  password: 'Test12345!',
+  confirmPassword: 'Test12345!',
 }
 
-describe('useSendPasswordResetRequest', () => {
+describe('useSubmitPasswordReset', () => {
+  beforeEach(() => {
+    vi.resetAllMocks()
+    mockUseParams.mockReturnValue({ token })
+  })
   it('should call the hook successfully', async () => {
     mockAxiosPost.mockResolvedValueOnce({
       status: 200,
@@ -33,7 +43,7 @@ describe('useSendPasswordResetRequest', () => {
       setIsLoading: mockSetIsLoading,
     })
 
-    const { result } = renderHook(() => useSendPasswordResetRequest())
+    const { result } = renderHook(() => useSubmitPasswordReset())
 
     await act(async () => {
       await result.current(mockData)
@@ -41,13 +51,46 @@ describe('useSendPasswordResetRequest', () => {
 
     expect(mockSetIsLoading).toHaveBeenNthCalledWith(1, true)
     expect(mockAxiosPost).toHaveBeenCalledWith(
-      'https://mockapi.com/auth/forgot-password',
+      'https://mockapi.com/auth/reset-password',
       mockData,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
     )
     expect(mockSetStatusMessage).toHaveBeenCalledWith(
-      'If an account with that email exists, a password reset link has been sent. Please check your inbox.',
+      'Your password was updated successfully. Go back to the sign in page.',
     )
     expect(mockSetIsLoading).toHaveBeenNthCalledWith(2, false)
+  })
+
+  it('should not call the hook if the passwords dont match', async () => {
+    const mockDifferentData = {
+      password: 'Teste12345!',
+      confirmPassword: 'Test12345!',
+    }
+
+    process.env.NEXT_PUBLIC_API_URL = 'https://mockapi.com'
+
+    const mockSetStatusMessage = vi.fn()
+    const mockSetIsLoading = vi.fn()
+    mockUseGeneralStore.mockReturnValue({
+      setStatusMessage: mockSetStatusMessage,
+      setIsLoading: mockSetIsLoading,
+    })
+
+    const { result } = renderHook(() => useSubmitPasswordReset())
+
+    await act(async () => {
+      await result.current(mockDifferentData)
+    })
+
+    expect(mockSetIsLoading).not.toHaveBeenCalled()
+    expect(mockAxiosPost).not.toHaveBeenCalled()
+    expect(mockSetStatusMessage).toHaveBeenCalledWith(
+      'Passwords must be the same!',
+    )
   })
 
   it('mocks a situation where the api returns an error', async () => {
@@ -62,7 +105,7 @@ describe('useSendPasswordResetRequest', () => {
       setIsLoading: mockSetIsLoading,
     })
 
-    const { result } = renderHook(() => useSendPasswordResetRequest())
+    const { result } = renderHook(() => useSubmitPasswordReset())
 
     await act(async () => {
       await result.current(mockData)
@@ -70,8 +113,13 @@ describe('useSendPasswordResetRequest', () => {
 
     expect(mockSetIsLoading).toHaveBeenNthCalledWith(1, true)
     expect(mockAxiosPost).toHaveBeenCalledWith(
-      'https://mockapi.com/auth/forgot-password',
+      'https://mockapi.com/auth/reset-password',
       mockData,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
     )
     expect(mockAxiosErrorHandler).toHaveBeenCalledWith({
       error: mockedError,
