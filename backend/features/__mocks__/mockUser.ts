63b1d1be1167111e8aa1c4aff1349e1cae13c95c
Arthur Araujo <arthurtute200@hotmail.com>
2025-11-17 19:45:04 -0300

    test: implementing unitary testing for signInService

---
+++ backend/features/__mocks__/mockUser.ts
@@ -0,0 +1,8 @@
+export const mockUser = {
+  id: 'c4b8390d-3c2c-4e8e-9f4d-5c6b84ccf3b9',
+  email: 'example@email.com',
+  firstName: 'John',
+  lastName: '',
+  password: '$2b$10$cmO2pzsI4aG5oQ13NtIlW.gvuu5M.0lDa/ofsSIoYD/hffzzTenpi', // hash for password 'password'
+  createdAt: '2025-01-01'
+}
