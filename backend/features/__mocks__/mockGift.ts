698056c3c48c405b05a8bb60535edd3b7c68f107
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-09 20:04:04 -0300

    test: commiting the created mocks for unitary tests

---
+++ backend/features/__mocks__/mockGift.ts
@@ -0,0 +1,8 @@
+export const mockGift = {
+  productName: 'Product1',
+  productLink: 'www.product1.com',
+  quantity: 2,
+  giftID: 1,
+  fromWedding: 1,
+  createdAt: '2025-01-01',
+};
--- backend/features/__mocks__/mockUser.ts
+++ backend/features/__mocks__/mockUser.ts
@@ -1,3 +1,5 @@
+import { mockWedding } from './mockWedding';
+
 export const mockUser = {
   id: 'c4b8390d-3c2c-4e8e-9f4d-5c6b84ccf3b9',
   email: 'example@email.com',
@@ -5,4 +7,5 @@ export const mockUser = {
   lastName: '',
   password: '$2b$10$cmO2pzsI4aG5oQ13NtIlW.gvuu5M.0lDa/ofsSIoYD/hffzzTenpi', // hash for password 'password'
   createdAt: '2025-01-01',
+  weddingsOwn: [mockWedding],
 };
