63a7d146c1d081525d17ae1f84035a67416816cd
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-26 19:08:58 -0300

    feat: created auth domain router

---
+++ api/src/routes/auth-routes.ts
@@ -0,0 +1,6 @@
+import { signInController } from '@/controllers/auth/sign-in.js';
+import express, { type Router } from 'express';
+
+export const authRouter: Router = express.Router();
+
+authRouter.post('/sign-in', signInController);
