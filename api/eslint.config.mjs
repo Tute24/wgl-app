209ebb8e9d10314f110066188f2dc4c49aebe44a
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-25 21:13:40 -0300

    chore: implementing eslint and prettier config

---
+++ api/eslint.config.mjs
@@ -0,0 +1,52 @@
+import js from '@eslint/js'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default [
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      'coverage/**',
+      '.prisma/**',
+      'prisma/migrations/**',
+    ],
+  },
+
+  js.configs.recommended,
+
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+      'import/prefer-default-export': 'off',
+      'class-methods-use-this': 'off',
+      'no-undef': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+
+  // Desativa regras que entram em conflito com o Prettier
+  prettierConfig,
+]
