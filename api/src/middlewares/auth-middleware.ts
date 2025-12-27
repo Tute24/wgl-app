37d5880b3d32cd965d47d015b86c78486084999b
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-27 20:30:39 -0300

    refactor: refactoring authMiddleware after express request type module augmentation

--- api/src/middlewares/auth-middleware.ts
+++ api/src/middlewares/auth-middleware.ts
@@ -1,21 +1,10 @@
 import { env } from '@/env/index.js';
+import type { TokenPayload } from '@/types/auth/token-payload.js';
 import { AppError } from '@/utils/app-error.js';
 import { type Response, type Request, type NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
-interface TokenPayload {
-  id: string;
-}
-
-export interface CustomRequest extends Request {
-  authUser: TokenPayload;
-}
-
-export default function authMiddleware(
-  req: CustomRequest,
-  res: Response,
-  next: NextFunction,
-): void {
+export default function authMiddleware(req: Request, _res: Response, next: NextFunction): void {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) throw new AppError('Invalid credentials.', 401);
 
