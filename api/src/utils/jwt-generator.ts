49185cd422fd0fd7f0fa62a99431e7be1fd727ae
Arthur Araujo <arthurtute200@hotmail.com>
2026-01-05 17:09:25 -0300

    fix: fixing name of token property

--- api/src/utils/jwt-generator.ts
+++ api/src/utils/jwt-generator.ts
@@ -2,7 +2,7 @@ import { env } from '@/env/index';
 import jwt from 'jsonwebtoken';
 
 export function generateJwt(id: string) {
-  const token = jwt.sign({ payload: id }, env.SECRET_KEY, {
+  const token = jwt.sign({ id: id }, env.SECRET_KEY, {
     expiresIn: '1h',
   });
 
