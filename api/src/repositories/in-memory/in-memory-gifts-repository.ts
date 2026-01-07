c253b32b2b6b6a9e2a2b913471359298f187d2be
Arthur Araujo <arthurtute200@hotmail.com>
2026-01-07 18:51:47 -0300

    feat: updating in memory gifts repo

--- api/src/repositories/in-memory/in-memory-gifts-repository.ts
+++ api/src/repositories/in-memory/in-memory-gifts-repository.ts
@@ -1,6 +1,7 @@
 import type { CreateGiftsDto } from '@/dtos/gifts/create-gifts';
 import type { GiftsRepository } from '../gifts-repository';
 import type { Gift } from '@prisma/client';
+import { UpdateQuantityDto } from '@/dtos/gifts/update-quantity';
 
 export class InMemoryGiftsRepository implements GiftsRepository {
   public giftDb: Gift[] = [];
@@ -35,5 +36,14 @@ export class InMemoryGiftsRepository implements GiftsRepository {
   async deleteGift(giftId: number) {
     const weddingIndex = this.giftDb.findIndex((gift) => gift.id === giftId);
     this.giftDb.splice(weddingIndex, 1);
+  }
+
+  async updateGiftQuantity(data: UpdateQuantityDto) {
+    const gifts = this.giftDb.map((gift) =>
+      gift.id === data.giftId ? { ...gift, quantity: data.updatedQuantity } : gift,
+    );
+
+    const gift = gifts.find((gift) => gift.id === data.giftId);
+    return gift!;
   }
 }
