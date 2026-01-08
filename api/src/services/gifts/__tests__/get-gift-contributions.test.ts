24239fb09f794d203b24083cef5396757517ebb8
Arthur Araujo <arthurtute200@hotmail.com>
2026-01-08 16:06:24 -0300

    test: unit test for get gift contributions service

--- api/src/services/gifts/__tests__/get-gift-contributions.test.ts
+++ api/src/services/gifts/__tests__/get-gift-contributions.test.ts
@@ -1,13 +1,13 @@
-import { InMemoryGuestsRepository } from '@/repositories/in-memory/in-memory-guests-repository';
-import { RegisterGiftContributionService } from '../register-gift-contribution';
 import { InMemoryAuthRepository } from '@/repositories/in-memory/in-memory-auth-repository';
-import { InMemoryGiftsRepository } from '@/repositories/in-memory/in-memory-gifts-repository';
-import { InMemoryWeddingsRepository } from '@/repositories/in-memory/in-memory-weddings-repository';
 import { InMemoryGiftContributionsRepository } from '@/repositories/in-memory/in-memory-gift-contributions-repository';
+import { InMemoryGiftsRepository } from '@/repositories/in-memory/in-memory-gifts-repository';
+import { InMemoryGuestsRepository } from '@/repositories/in-memory/in-memory-guests-repository';
+import { InMemoryWeddingsRepository } from '@/repositories/in-memory/in-memory-weddings-repository';
+import { GetGiftContributionsService } from '../get-gift-contributions';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { userMock } from '@/services/auth/__mocks__/user-mock';
-import { hash } from 'bcryptjs';
 import { weddingMock } from '@/services/weddings/__mocks__/wedding-mock';
+import { hash } from 'bcryptjs';
 import { giftsMock } from '../__mocks__/gifts-mock';
 
 let authRepository: InMemoryAuthRepository;
@@ -15,9 +15,9 @@ let weddingsRepository: InMemoryWeddingsRepository;
 let guestsRepository: InMemoryGuestsRepository;
 let giftsRepository: InMemoryGiftsRepository;
 let giftContributionsRepository: InMemoryGiftContributionsRepository;
-let sut: RegisterGiftContributionService;
+let sut: GetGiftContributionsService;
 
-describe('RegisterGiftContribution', () => {
+describe('GetGiftContributionsService', () => {
   beforeEach(() => {
     authRepository = new InMemoryAuthRepository();
     guestsRepository = new InMemoryGuestsRepository();
@@ -27,16 +27,15 @@ describe('RegisterGiftContribution', () => {
       authRepository,
       giftsRepository,
     );
-    sut = new RegisterGiftContributionService(
+
+    sut = new GetGiftContributionsService(
       authRepository,
       weddingsRepository,
-      giftsRepository,
-      guestsRepository,
       giftContributionsRepository,
     );
   });
 
-  it('should successfully register a gift contribution', async () => {
+  it('should get gift contributions successfully', async () => {
     const user = await authRepository.createUser({
       email: userMock.email,
       firstName: userMock.firstName,
@@ -56,47 +55,23 @@ describe('RegisterGiftContribution', () => {
     await giftsRepository.createGifts(treatedGifts);
     const giftsList = await giftsRepository.getGiftsFromWedding(wedding.id);
 
-    const { message } = await sut.execute({
-      userId: user.id,
-      giftedQuantity: 1,
-      weddingId: wedding.id,
+    await giftContributionsRepository.registerGiftContribution({
       giftId: giftsList[0]!.id,
+      presenter: user.id,
+      quantity: 1,
+      relatedWedding: wedding.id,
     });
 
-    expect(message).toEqual('Gift contribution registered successfully.');
+    const { giftContributions, weddingHeader } = await sut.execute({
+      userId: user.id,
+      weddingId: wedding.id,
+    });
+
+    expect(giftContributions).toHaveLength(1);
+    expect(weddingHeader.weddingTitle).toEqual(weddingMock.weddingTitle);
   });
 
-  it('should not be able to complete request if the gifted quantity is bigger than the available gift quantity', async () => {
-    const user = await authRepository.createUser({
-      email: userMock.email,
-      firstName: userMock.firstName,
-      lastName: userMock.lastName,
-      password: await hash(userMock.password, 6),
-    });
-
-    const wedding = await weddingsRepository.createWedding({
-      createdBy: user.id,
-      weddingTitle: weddingMock.weddingTitle,
-      weddingDate: weddingMock.weddingDate,
-      shippingAddress: weddingMock.shippingAddress,
-    });
-
-    const treatedGifts = giftsMock.gifts.map((gift) => ({ ...gift, fromWedding: wedding.id }));
-
-    await giftsRepository.createGifts(treatedGifts);
-    const giftsList = await giftsRepository.getGiftsFromWedding(wedding.id);
-
-    await expect(
-      sut.execute({
-        userId: user.id,
-        giftedQuantity: 40,
-        weddingId: wedding.id,
-        giftId: giftsList[0]!.id,
-      }),
-    ).rejects.toThrow('Requested quantity exceeds available gift quantity.');
-  });
-
-  it('should not be able to complete request if the requesting user is not a guest or the owner of the wedding', async () => {
+  it('should not fetch contributions if user is not the wedding owner', async () => {
     const user1 = await authRepository.createUser({
       email: userMock.email,
       firstName: userMock.firstName,
@@ -123,12 +98,17 @@ describe('RegisterGiftContribution', () => {
     await giftsRepository.createGifts(treatedGifts);
     const giftsList = await giftsRepository.getGiftsFromWedding(wedding.id);
 
+    await giftContributionsRepository.registerGiftContribution({
+      giftId: giftsList[0]!.id,
+      presenter: user1.id,
+      quantity: 1,
+      relatedWedding: wedding.id,
+    });
+
     await expect(
       sut.execute({
         userId: user2.id,
-        giftedQuantity: 1,
         weddingId: wedding.id,
-        giftId: giftsList[0]!.id,
       }),
     ).rejects.toThrow('User does not have permission to perform this action.');
   });
@@ -137,9 +117,7 @@ describe('RegisterGiftContribution', () => {
     await expect(
       sut.execute({
         userId: 'id',
-        giftedQuantity: 1,
         weddingId: 1,
-        giftId: 1,
       }),
     ).rejects.toThrow('User not found.');
   });
@@ -155,34 +133,8 @@ describe('RegisterGiftContribution', () => {
     await expect(
       sut.execute({
         userId: user.id,
-        giftedQuantity: 1,
         weddingId: 1,
-        giftId: 1,
       }),
     ).rejects.toThrow('Wedding not found.');
-  });
-
-  it('should return 404 if gift does not exist', async () => {
-    const user = await authRepository.createUser({
-      email: userMock.email,
-      firstName: userMock.firstName,
-      lastName: userMock.lastName,
-      password: await hash(userMock.password, 6),
-    });
-
-    const wedding = await weddingsRepository.createWedding({
-      createdBy: user.id,
-      weddingTitle: weddingMock.weddingTitle,
-      weddingDate: weddingMock.weddingDate,
-      shippingAddress: weddingMock.shippingAddress,
-    });
-    await expect(
-      sut.execute({
-        userId: user.id,
-        giftedQuantity: 1,
-        weddingId: wedding.id,
-        giftId: 1,
-      }),
-    ).rejects.toThrow('Gift not found.');
   });
 });
