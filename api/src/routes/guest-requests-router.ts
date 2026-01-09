7885c00a56fc61f9fda2ca36710d76efe05af64e
Arthur Araujo <arthurtute200@hotmail.com>
2026-01-09 19:28:00 -0300

    feat: implemented the create guest request router functionality

--- api/src/routes/guest-requests-router.ts
+++ api/src/routes/guest-requests-router.ts
@@ -1,3 +1,7 @@
+import { createGuestRequestController } from '@/controllers/guest-requests/create-guest-request';
+import authMiddleware from '@/middlewares/auth-middleware';
 import express, { Router } from 'express';
 
 export const guestRequestsRouter: Router = express.Router();
+
+guestRequestsRouter.post('/create', authMiddleware, createGuestRequestController);
---
+++ api/src/services/guest-requests/create-guest-request.ts
@@ -0,0 +1,39 @@
+import { CreateGuestRequestDto } from '@/dtos/guest-requests/create-guest-request';
+import { AuthRepository } from '@/repositories/auth-repository';
+import { GuestRequestsRepository } from '@/repositories/guest-requests-repository';
+import { WeddingsRepository } from '@/repositories/weddings-repository';
+import { AppError } from '@/utils/app-error';
+
+export class CreateGuestRequestService {
+  constructor(
+    private authRepository: AuthRepository,
+    private weddingsRepository: WeddingsRepository,
+    private guestRequestsRepository: GuestRequestsRepository,
+  ) {}
+
+  async execute({ userId, weddingId }: CreateGuestRequestDto): Promise<{ message: string }> {
+    const user = await this.authRepository.findById(userId);
+    if (!user) throw new AppError('User not found.', 404);
+
+    const wedding = await this.weddingsRepository.findWeddingById(weddingId);
+    if (!wedding) throw new AppError('Wedding not found.', 404);
+
+    if (wedding.createdBy === user.id) throw new AppError('User is the wedding owner.', 409);
+
+    const pendingRequests = await this.guestRequestsRepository.findPendingRequestsByUserAndWedding(
+      userId,
+      weddingId,
+    );
+
+    await this.guestRequestsRepository.createRequest({
+      userId,
+      weddingId,
+    });
+
+    if (pendingRequests.length > 0)
+      throw new AppError('User currently has a pending guest request for this wedding', 409);
+    return {
+      message: 'Request created successfully.',
+    };
+  }
+}
