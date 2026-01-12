f64fb6aca78388c63fe6e53b81f87067fcd5ea45
Arthur Araujo <arthurtute200@hotmail.com>
2026-01-12 17:38:18 -0300

    feat: implemented the count pending guest requests route functionality

---
+++ api/src/dtos/guest-requests/count-pending-guest-requests.ts
@@ -0,0 +1,3 @@
+export interface CountPendingGuestRequestsDto {
+  userId: string;
+}
--- api/src/routes/guest-requests-router.ts
+++ api/src/routes/guest-requests-router.ts
@@ -1,4 +1,5 @@
 import { acceptGuestRequestController } from '@/controllers/guest-requests/accept-guest-request';
+import { countPendingGuestRequestsController } from '@/controllers/guest-requests/count-pending-guest-requests';
 import { createGuestRequestController } from '@/controllers/guest-requests/create-guest-request';
 import { denyGuestRequestController } from '@/controllers/guest-requests/deny-guest-request';
 import { getGuestRequestsHistoryController } from '@/controllers/guest-requests/get-guest-requests-history';
@@ -11,3 +12,4 @@ guestRequestsRouter.post('/create', authMiddleware, createGuestRequestController
 guestRequestsRouter.get('/', authMiddleware, getGuestRequestsHistoryController);
 guestRequestsRouter.patch('/accept', authMiddleware, acceptGuestRequestController);
 guestRequestsRouter.patch('/deny', authMiddleware, denyGuestRequestController);
+guestRequestsRouter.get('/pending/count', authMiddleware, countPendingGuestRequestsController);
