8200a51f3013bbdf10f81709694313577d4eb255
Arthur Araujo <arthurtute200@hotmail.com>
2026-01-13 20:55:26 -0300

    docs: implemented swagger ui with express on app.ts

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,6 +6,8 @@ import { authRouter } from './routes/auth-router';
 import { weddingsRouter } from './routes/weddings-router';
 import { giftsRouter } from './routes/gifts-router';
 import { guestRequestsRouter } from './routes/guest-requests-router';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerDefinition } from './docs/swagger';
 
 dotenv.config();
 app.use(express.json());
@@ -13,7 +15,7 @@ app.use('/auth', authRouter);
 app.use('/weddings', weddingsRouter);
 app.use('/gifts', giftsRouter);
 app.use('/guest-requests', guestRequestsRouter);
-
+app.use('/wgl-app-api-swagger', swaggerUi.serve, swaggerUi.setup(swaggerDefinition));
 const port = env.PORT || 3333;
 
 app.listen(port, () => {
