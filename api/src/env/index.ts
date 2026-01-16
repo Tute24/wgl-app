337ee12f8cffe583da1a967da4aea7b2440f37be
Arthur Araujo <arthurtute200@hotmail.com>
2026-01-16 19:11:23 -0300

    fix: fixing zod env variables validation file structure

--- api/src/env/index.ts
+++ api/src/env/index.ts
@@ -3,31 +3,25 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const baseSchema = {
+  NODE_ENV: z.enum(['development', 'production', 'test']),
+  SECRET_KEY: z.string(),
+};
+
 const envSchema = z.object({
+  ...baseSchema,
   DATABASE_URL: z.string(),
   DIRECT_URL: z.string(),
   PORT: z.coerce.number().default(3333),
-  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
-  SECRET_KEY: z.string(),
   NODEMAILER_EMAIL: z.email(),
   NODEMAILER_APP_PASSWORD: z.string(),
   FRONTEND_URL: z.string(),
 });
 
 const testSchema = z.object({
-  NODE_ENV: z.enum(['development', 'production', 'test']),
-  SECRET_KEY: z.string(),
+  ...baseSchema,
 });
 
-const _env =
-  process.env.NODE_ENV === 'test'
-    ? testSchema.safeParse(process.env)
-    : envSchema.safeParse(process.env);
+const isTest = process.env.NODE_ENV === 'test';
 
-if (_env.success === false) {
-  console.error('Invalid enviroment variables', _env.error.message);
-
-  throw new Error('Invalid enviroment variables');
-}
-
-export const env = _env.data;
+export const env = isTest ? testSchema.parse(process.env) : envSchema.parse(process.env);
