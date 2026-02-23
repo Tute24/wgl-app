5d515f7a9ebc26224f693c750a72e51a4782c871
Arthur Araujo <arthurtute200@hotmail.com>
2026-02-23 18:58:08 -0300

    chore: created config.py for env variables validation

---
+++ api-python/src/core/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV_PATH = Path(__file__).resolve().parents[2] / ".env"
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=ENV_PATH)
+
+    DATABASE_URL: str
+
+settings = Settings() # type: ignore
